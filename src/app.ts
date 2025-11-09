69faafff5d4719de9f7f2e918d1474895610f976
Didiercito <231193@ids.upchiapas.edu.mx>
2025-11-08 23:13:46 -0600

    cors

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ import scheduleRoutes from './infrastructure/api/routes/schedule.routes';
 import reputationRoutes from './infrastructure/api/routes/reputation.routes';
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
